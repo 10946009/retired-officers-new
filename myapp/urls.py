5e61fba5ee6584cb534d88a7e657975d245b4fe6
品 <10946009@ntub.edu.tw>
2024-03-21 00:39:40 +0800

    feat: change register & join process

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,18 +2,16 @@ from myapp.views.index import index
 from myapp.views.register import register
 from myapp.views.student_login import student_login
 from myapp.views.logout import logout
-from myapp.views.student_join import student_join
 from myapp.views.student_print_sign_up import student_print_sign_up
 from myapp.views.student_print_score import student_print_score
-from myapp.views.student_edit import student_edit
+from myapp.views.student_join import student_join
 from django.urls import path , include
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', index, name='index'),
     path('register/', register, name='register'),
     path('student_login/', student_login, name='student_login'),
-    path('logout/', logout, name='logout'),
-    path('student_edit/', student_edit, name='student_edit'),
+    path('logout/', logout, name='user_logout'),
     # about activity
     path('student_join/<int:activity_id>', student_join, name='student_join'),
     path('student_print_sign_up/<int:activity_id>', student_print_sign_up, name='student_print_sign_up'),
@@ -23,26 +21,10 @@ urlpatterns = [
 # accounts/ ^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$ [name='account_reset_password_from_key']
 # accounts/ password/reset/key/done/ [name='account_reset_password_from_key_done']
     # path('password/reset',student_password_reset,name='student_password_reset'),
-    path('accounts/password/reset',include('django.contrib.auth.urls')),
-    # path('accounts/password/reset',include('django.contrib.auth.urls')),
-#------------------------------ 
-    # path('create', views.create, name='create'),
-    # path('list', views.list, name='list'),
-    # path('fileupload', views.fileupload, name='fileupload'),
-    # path('edit/<int:id>', views.edit, name='edit'),
-    # path('edit/update/<int:id>', views.update, name='update'),
-    # path('delete/<int:id>', views.delete, name='delete'),
-    # path('ajax/', views.ajax, name='ajax'),
-    # path('ajax/ajax', views.ajax, name='ajaxpost'),
-    # path('ajax/delete', views.ajax_delete, name='ajax_delete'),
-    # path('ajax/getajax', views.getajax, name='getajax'),
-
-    # path('register/success/', views.register_success, name='register_success'),
-    # path('users/', views.users, name='users'),
-    # path('users/delete/<int:id>', views.user_delete, name='user_delete'),
-    # path('upload/csv/', views.upload_csv, name='upload_csv'),
-    # path('change_password', views.changePassword, name='changePassword'),
-    # path('file/delete', views.changePassword, name='changePassword'),
-    # path('file/delete/<int:id>', views.deleteFiles, name='deleteFiles'),
-
+    path('accounts/password/reset',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('accounts/password/reset/done',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('accounts/password/reset/key/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+     name='password_reset_confirm'),
+    path('accounts/password/reset/key/done',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
--- myapp/views/student_join.py
+++ myapp/views/student_join.py
@@ -2,21 +2,39 @@ from datetime import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from myapp.models import Activity 
+#forms
+from myapp.forms import UserEditForm, StudentForm
 
 @login_required(login_url="/student_login")
-def student_join(request, activity_id):
-    user = request.user
-    print(user.id,activity_id)
-    if user.is_authenticated:
-        
-        if not hasattr(user, 'student'):
+def student_join(request,activity_id):
+    if request.user.is_authenticated:
+        # 定義表單
+        user_form = UserEditForm(instance=request.user)
+        # 如果使用者使用Google登入 沒有student的話，就建立一個student
+        if hasattr(request.user, 'student'):
+            student_form = StudentForm(instance=request.user.student)
+        else:
+            student_form = StudentForm()
 
-            return redirect("/student_edit")
-        
-        activity = Activity.objects.get(id=activity_id)
         if request.method == "POST":
-            user.student.activity.add(activity)
-            user.student.join_time = datetime.now()
-            user.save()
-
-    return redirect("/")
+            user_form = UserEditForm(request.POST, instance=request.user)
+            if hasattr(request.user, 'student'):
+                student_form = StudentForm(request.POST, request.FILES, instance=request.user.student)
+            else:
+                #create a new student
+                student_form = StudentForm(request.POST, request.FILES)
+            if user_form.is_valid() and student_form.is_valid():
+                user = user_form.save()
+                student = student_form.save(commit=False)
+                student.user = user
+                student.save()
+                if request.POST.get("saveValue") == "0":
+                    activity = Activity.objects.get(id=activity_id)
+                    user.student.activity.add(activity)
+                    user.student.join_time = datetime.now()
+                    user.save()
+                    return render(request, "message.html", {"next": "/", "message": "報名成功!至首頁列印報名表"})
+                else:
+                    return render(request, "message.html", {"next": f"/student_join/{activity_id}", "message": "儲存成功"})
+    content = {"user_form": user_form, "student_form": student_form, "activity_id": activity_id}
+    return render(request, "student_join.html", content)
