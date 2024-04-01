52735bb6728d62b482f6fa21e7a4b22f4846a720
品 <10946009@ntub.edu.tw>
2024-04-01 13:50:58 +0800

    feat:add password judge

--- myapp/views/register.py
+++ myapp/views/register.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render,redirect
-from myapp.forms import UserForm, StudentForm
-
+from myapp.forms import UserForm
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 def register(request):
     if request.method == "POST":
         user_form = UserForm(request.POST)
 
-        print(user_form.is_valid())
-        print(user_form.errors)
         if user_form.is_valid():
+            password = user_form.cleaned_data.get('password')
+
+            try:
+                validate_password(password)
+            except ValidationError as e:
+                user_form.add_error('password', e)
+
+            if user_form.errors:
+                content = {"user_form": user_form}
+                return render(request, "register.html", content)
+
             user = user_form.save()
             user.set_password(user.password)
             user.save()
