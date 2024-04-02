400924c2b6226989149abf2b04584986e8476883
品 <10946009@ntub.edu.tw>
2024-04-02 20:25:24 +0800

    feat: sh fix

--- django.sh
+++ django.sh
@@ -2,7 +2,7 @@
 
 # Collect static files
 echo "Collect static files"
-python manage.py collectstatic --noinput
+poetry run python manage.py collectstatic --noinput
 
 # Start server
 echo "Starting server"
