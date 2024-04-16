fd8de4c88e99d634683b5fa68dcc12820c971628
品 <10946009@ntub.edu.tw>
2024-04-16 21:18:50 +0800

    feat:change index roc year

--- admin_panel/templatetags/zip.py
+++ admin_panel/templatetags/zip.py
@@ -9,4 +9,5 @@ def zip_lists(list1, list2):
 # 自訂過濾器：將西元年轉換為民國年
 @register.filter(name='to_roc_year')
 def to_roc_year(date):
-    return f'{date.year - 1911}年{date.month}月{date.day}日 {date.hour}:{date.minute}'
+    # 113年4月3日 07:59
+    return f'{date.year-1911}年{date.month}月{date.day}日 {date.strftime("%H:%M")}'
