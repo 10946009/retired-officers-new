df31d9abda181ea211aadca6a0582f71ffaa102b
品 <10946009@ntub.edu.tw>
2024-04-17 10:51:51 +0800

    feat: time bug

--- admin_panel/templatetags/zip.py
+++ admin_panel/templatetags/zip.py
@@ -1,13 +1,22 @@
 from django import template
-
+from django.utils import timezone
 register = template.Library()
+
 
 @register.filter
 def zip_lists(list1, list2):
     return zip(list1, list2)
 
+
 # 自訂過濾器：將西元年轉換為民國年
-@register.filter(name='to_roc_year')
+@register.filter(name="to_roc_year")
 def to_roc_year(date):
-    # 113年4月3日 07:59
-    return f'{date.year-1911}年{date.month}月{date.day}日 {date.strftime("%H:%M")}'
+    # Make sure the date is timezone-aware
+    if timezone.is_naive(date):
+        date = timezone.make_aware(date)
+
+    # Convert the date to the current timezone
+    date = timezone.localtime(date)
+
+    # Format the date
+    return f"{date.year-1911}年{date.month}月{date.day}日 {date.strftime('%H:%M')}"
