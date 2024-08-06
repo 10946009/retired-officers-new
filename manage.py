ca0a4280601c888b07fa345c760fa365646262d4
品 <10946009@ntub.edu.tw>
2024-08-06 13:15:52 +0800

    feat: runserver check group "admin-school"

--- manage.py
+++ manage.py
@@ -3,12 +3,32 @@
 import os
 import sys
 
+def check_admin_school_group():
+    import django
+    django.setup()
+    from django.contrib.auth.models import Group, Permission
+    group_name = 'admin-school'
+    group, created = Group.objects.get_or_create(name=group_name)
+    if not Group.objects.filter(name=group_name).exists():
+        Group.objects.create(name=group_name)
+        print(f'Successfully created group "{group_name}"')
+            # 添加 myapp.view_activity 權限
+    try:
+        permission = Permission.objects.get(codename='view_activity', content_type__app_label='myapp')
+        group.permissions.add(permission)
+        print(f'Assigned permission "{permission.name}" to group "{group_name}"')
+    except Permission.DoesNotExist:
+        print(f'Permission "view_activity" not found in app "myapp"')
+    else:
+        print(f'Group "{group_name}" already exists')
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
         from django.core.management import execute_from_command_line
+        check_admin_school_group()
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
